f102f53f0b5dd53f48acce124b69461bfff69c7d
IsunzaGuillermo <isunzaguillermo@gmail.com>
2022-09-02 16:46:28 -0500

    Se agrega el ejercicio de Clases Abstractas de JAVA

---
+++ AbstractAnimals/src/abstractAnimal/TestAnimals.java
@@ -0,0 +1,29 @@
+package abstractAnimal;
+
+public class TestAnimals {
+
+	public static void main(String[] args) {
+		
+		Frog Bullfrog = new Frog();
+		Bullfrog.move();
+		Bullfrog.calls();		
+		
+		Vulture TurkeyVulture = new Vulture();
+		TurkeyVulture.move();
+		TurkeyVulture.calls();	
+		
+		Crocodile Moreletii = new Crocodile();
+		Moreletii.move();
+		Moreletii.calls();	
+		
+		Gibbon Siamang = new Gibbon();
+		Siamang.move();
+		Siamang.calls();	
+		
+		Lamprey SeaLamprey = new Lamprey();
+		SeaLamprey.move();
+		SeaLamprey.calls();	
+		
+	}//main
+
+}//class
